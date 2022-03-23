154113c5bde00f63fe07ecaa1646c521becaa2df
Frederic Marazzato <marazzato@lsu.edu>
2022-03-23 18:28:34 -0500

    Added a plot of the L2 error.

--- surfing/plot.py
+++ surfing/plot.py
@@ -2,12 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 CG = np.loadtxt('CG_CG_1/perf.txt')
-DG = np.loadtxt('DG_CR_1/perf.txt')
+DG_CR = np.loadtxt('DG_CR_1/perf.txt')
+DG_CG = np.loadtxt('DG_CG_1/perf.txt')
 
 tot_CG = np.cumsum(CG[:,0])
-tot_DG = np.cumsum(DG[:,0])
+tot_DG_CR = np.cumsum(DG_CR[:,0])
+tot_DG_CG = np.cumsum(DG_CG[:,0])
 
 plt.plot(tot_CG, label='CG')
-plt.plot(tot_DG, label='DG')
+plt.plot(tot_DG_CR, label='DG CR')
+plt.plot(tot_DG_CG, label='DG CG')
 plt.legend(loc='upper left')
+plt.title('Total number of iterations')
+plt.show()
+
+plt.plot(CG[:,1], label='CG')
+plt.plot(DG_CR[:,1], label='DG CR')
+plt.plot(DG_CG[:,1], label='DG CG')
+plt.legend(loc='upper left')
+plt.title(r'$L^2$ error')
 plt.show()
