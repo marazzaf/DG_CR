9c42e05d8da42953cb367b54b657d499e863cde4
marazzaf <frederic.marazzato@ponts.org>
2022-03-23 15:20:48 -0500

    Added a file to plot the perf results.

---
+++ surfing/plot.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+CG = np.loadtxt('CG_CG_1/perf.txt')
+DG = np.loadtxt('DG_CR_1/perf.txt')
+
+tot_CG = np.cumsum(CG[:,0])
+tot_DG = np.cumsum(DG[:,0])
+
+plt.plot(tot_CG, label='CG')
+plt.plot(tot_DG, label='DG')
+plt.legend(loc='upper left')
+plt.show()
