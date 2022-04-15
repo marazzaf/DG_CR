de57f44147e967db9773a2ba7b0b9ed1c249a723
Frederic Marazzato <marazzato@lsu.edu>
2022-04-15 14:42:28 -0500

    Got the convergence orders.

--- surfing/conv.py
+++ surfing/conv.py
@@ -8,12 +8,18 @@ c_2 = np.loadtxt('conv_2/conv.txt')
 c_3 = np.loadtxt('conv_3/conv.txt')
 c_4 = np.loadtxt('conv_4/conv.txt')
 
-n_0 = 110605
-n_1 = 248030
-n_2 = 511597
-n_3 = 1057744
-n_4 = 2028499
+n = np.array([110605, 248030, 511597, 1057744, 2028499])
+err = np.array([c_0[:,1], c_1[:,1], c_2[:,1], c_3[:,1], c_4[:,1]])
+err_4 = err[:,0]
+err_6 = err[:,1]
+err_8 = err[:,2]
 
+conv_4 = 2 * np.log(err_4[:-1] / err_4[1:]) / np.log(n[1:] / n[:-1])
+print(conv_4)
+conv_6 = 2 * np.log(err_6[:-1] / err_6[1:]) / np.log(n[1:] / n[:-1])
+print(conv_6)
+conv_8 = 2 * np.log(err_8[:-1] / err_8[1:]) / np.log(n[1:] / n[:-1])
+print(conv_8)
 sys.exit()
 
 plt.plot(CG_1[:,1],tot_CG_1, label='CG CG 1')
