f68849010f53f9c95faab90ecff3a4c8a7d82ef8
Frederic Marazzato <marazzato@lsu.edu>
2022-08-17 10:33:10 -0500

    Updated the plots of the ld curves.

--- beam/plot.py
+++ beam/plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 ld = np.loadtxt('cluster/ld.txt')
 
 plt.plot(ld[:,0], abs(ld[:,1]), '-')
-plt.xlim(0,0.12)
+plt.xlim(0,0.06)
 plt.ylim(0,0.05)
 plt.xlabel('Displacement [mm]')
 plt.ylabel('Force [kN]')
--- holes/plot.py
+++ holes/plot.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ld = np.loadtxt('cluster/ld.txt')
-
-plt.plot(ld[:,0], abs(ld[:,1]), '-')
-plt.xlim(0,2.5)
+x = np.arange(0, 0.201, 1e-2)
+plt.plot(x, 0.44125/0.2*x, '-', color='blue')
+plt.plot(ld[:,0], abs(ld[:,1]), '-', color='blue')
+plt.xlim(0,1.25)
 plt.ylim(0,0.7)
 plt.xlabel('Displacement [mm]')
 plt.ylabel('Force [kN]')
