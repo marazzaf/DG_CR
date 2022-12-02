0f56bfe396c5c9d6c414e96dde9f24f534cb506f
marazzato <marazzato@system76-pc.localdomain>
2022-12-02 17:38:12 -0600

    These results are good. Try to lower \ell / h and see what happens.

--- surfing_new/plot.py
+++ surfing_new/plot.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-CG_2 = np.loadtxt('test_CG_2/energies.txt')
-DG_2 = np.loadtxt('test_DG_2/energies.txt')
-CG_3 = np.loadtxt('test_CG_3/energies.txt')
+CG_4 = np.loadtxt('test_CG_44/energies.txt')
+DG_4 = np.loadtxt('test_DG_4/energies.txt')
+CG_3 = np.loadtxt('test_CG_33/energies.txt')
 DG_3 = np.loadtxt('test_DG_3/energies.txt')
 
-plt.plot(CG_2[:,0], CG_2[:,-2], '-*', label='CG 2')
-plt.plot(DG_2[:,0], DG_2[:,-2], '-*', label='DG 2')
+plt.plot(CG_4[:,0], CG_4[:,-2], '-*', label='CG 4')
+plt.plot(DG_4[:,0], DG_4[:,-2], '-*', label='DG 4')
 plt.plot(CG_3[:,0], CG_3[:,-2], '-*', label='CG 3')
 plt.plot(DG_3[:,0], DG_3[:,-2], '-*', label='DG 3')
 plt.legend()
