1163271fcaa64f31009ec995a159e7130f912ad4
marazzaf <frederic.marazzato@ponts.org>
2022-12-07 15:42:49 -0600

    New plot for paper.

--- surfing_new/plot.py
+++ surfing_new/plot.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-CG_4 = np.loadtxt('test_CG_44/energies.txt')
-DG_4 = np.loadtxt('test_DG_4/energies.txt')
-CG_3 = np.loadtxt('test_CG_33/energies.txt')
-DG_3 = np.loadtxt('test_DG_3/energies.txt')
+DG_2 = np.loadtxt('l_h_2/test_DG_4/energies.txt')
+DG_5 = np.loadtxt('l_h_5/test_DG_4/energies.txt')
+CG_2 = np.loadtxt('l_h_2/test_CG_44/energies.txt')
+CG_5 = np.loadtxt('l_h_5/test_CG_44/energies.txt')
 
-plt.plot(CG_4[:,0], CG_4[:,-2], '-*', label='CG 4')
-plt.plot(DG_4[:,0], DG_4[:,-2], '-*', label='DG 4')
-plt.plot(CG_3[:,0], CG_3[:,-2], '-*', label='CG 3')
-plt.plot(DG_3[:,0], DG_3[:,-2], '-*', label='DG 3')
+plt.plot(DG_2[:,0], DG_2[:,-2], '-*', label='DG $\ell/h=2$', color='#1f77b4')
+plt.plot(DG_5[:,0], DG_5[:,-2], '-o', label='DG $\ell/h=5$', color='#1f77b4')
+plt.plot(CG_2[:,0], CG_2[:,-2], '-*', label='CG $\ell/h=2$', color='#ff7f0e')
+plt.plot(CG_5[:,0], CG_5[:,-2], '-o', label='CG $\ell/h=5$', color='#ff7f0e')
+plt.xlim((0.25,0.7))
 plt.legend()
+plt.savefig('test.pdf')
 plt.show()
